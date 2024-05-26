990a6365500985ab7d3f76d0f55cbe76961687c4
luis.ortiz <luis.ortiz@nuvu.cc>
2024-05-26 20:30:16 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import PhoneInput, { isValidPhoneNumber } from 'react-phone-number-input/input'
+import cn from 'classnames'
+import 'react-phone-number-input/style.css'
+
+function App() {
+  
+   const [value, setValue] = useState<any>('+57')
+   const isValidValue = value && isValidPhoneNumber(value)
+   return (
+      <div className='p-5 bg-gray-100 min-h-screen'>
+         <div className="p-5 max-w-md mx-auto bg-white rounded-md">
+            <span>Enviar mensaje a Whatsapp</span>
+            <PhoneInput
+               className='mt-2 w-full border-2 border-gray-500 p-3 rounded-md'
+               placeholder=""
+               value={value}
+               onChange={setValue}
+            />
+
+            <a
+               className={cn('mt-2 p-3 block bg-green-500 text-white text-center rounded-md', {
+                  'bg-gray-500 cursor-not-allowed': !isValidValue
+               })}
+               href={isValidValue ? `https://api.whatsapp.com/send?phone=${value}` : '#'}
+            >Enviar
+            </a>
+         </div>
+      </div>
+
+   )
+}
+
+export default App
