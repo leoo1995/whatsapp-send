9c23a64a95aeda7e165110df0a44d85a8c06ae77
luis.ortiz <luis.ortiz@nuvu.cc>
2024-05-26 21:14:51 +0000

    bg color disabled

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
    const [value, setValue] = useState<any>('+57')
    const isValidValue = value && isValidPhoneNumber(value)
    return (
-      <div className='p-5 bg-gray-100 min-h-screen'>
+      <div className='p-5 bg-gray-100 min-h-screen flex items-center'>
          <div className="p-5 max-w-md mx-auto bg-white rounded-md">
             <span>Enviar mensaje a Whatsapp</span>
             <PhoneInput
@@ -20,7 +20,7 @@ function App() {
 
             <a
                className={cn('mt-2 p-3 block bg-green-500 text-white text-center rounded-md', {
-                  'bg-gray-500 cursor-not-allowed': !isValidValue
+                  'bg-gray-400 cursor-not-allowed': !isValidValue
                })}
                href={isValidValue ? `https://api.whatsapp.com/send?phone=${value}` : '#'}
             >Enviar
